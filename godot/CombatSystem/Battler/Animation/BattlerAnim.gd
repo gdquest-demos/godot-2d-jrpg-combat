e7d6ddfe31e7460b8b5038143670397077c96161
Nathan Lovato <nathan@gdquest.com>
2020-09-17 08:29:30 -0600

    Fix character getting stuck when taking damage
    Fix #2

--- godot/CombatSystem/Battler/Animation/BattlerAnim.gd
+++ godot/CombatSystem/Battler/Animation/BattlerAnim.gd
@@ -16,6 +16,7 @@ export (Direction) var direction := Direction.RIGHT setget set_direction
 var _position_start := Vector2.ZERO
 
 onready var anim_player: AnimationPlayer = $Pivot/AnimationPlayer
+onready var anim_player_damage: AnimationPlayer = $Pivot/AnimationPlayerDamage
 onready var tween: Tween = $Tween
 onready var anchor_front: Position2D = $FrontAnchor
 onready var anchor_top: Position2D = $TopAnchor
@@ -26,12 +27,21 @@ func _ready() -> void:
 
 
 func play(anim_name: String) -> void:
-	anim_player.play(anim_name)
-	anim_player.seek(0.0)
+	if anim_name == "take_damage":
+		anim_player_damage.play(anim_name)
+		anim_player_damage.seek(0.0)
+	else:
+		anim_player.play(anim_name)
+
+
+func is_playing() -> bool:
+	return anim_player.is_playing()
 
 
 func queue_animation(anim_name: String) -> void:
 	anim_player.queue(anim_name)
+	if not anim_player.is_playing():
+		anim_player.play()
 
 
 func get_front_anchor_global_position() -> Vector2:
